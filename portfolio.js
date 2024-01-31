2d87e73083aa57401bbc4d7053d662621935fe8e
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-01-31 11:41:13 +0000

    started adding styling classes to html within js file

--- portfolio.js
+++ portfolio.js
@@ -5,12 +5,16 @@ fetch('portfolio.json').then(function (response) {
 }).then(function (data) {
     data.projects.forEach(function (project) {
     portfolioDisplay.innerHTML += 
-        `<img src="${project.image}"> 
-        <h4>${project.title}</h4>
-        <div> 
-            <p>Deliverables</p>
-            <p>${project.deliverables}</p>
-        </div>
-        <a href="#">View Project</a>`
+        `<div class=" flex flex-col px-20 h-screen" >
+            <img class="object-contain m-auto" src="${project.image}"> 
+            <div class= "flex flex-wrap justify-between"> 
+                <h4 class="text-pink-500 text-lg font-extrabold">${project.title}</h4>
+                <div class="flex flex-col"> 
+                    <p>Deliverables</p>
+                    <p>${project.deliverables}</p>
+                </div>
+                <a href="#">View Project</a>
+                </div>
+            </div>`
     })
 })
