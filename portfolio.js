a483175a89a5fa1c43847d00769ed931641921b0
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-01-31 11:02:00 +0000

    fixed fetch request for portfolio.json

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,15 @@
-const portfolioDisplay = document.querySelector('portfolio')
+const portfolioDisplay = document.querySelector('.portfolio')
 
 fetch('portfolio.json').then(function (response) {
     return response.json()
 }).then(function (data) {
     data.projects.forEach(function (project) {
     portfolioDisplay.innerHTML += 
-        `<img src="${projects.image}"> 
-        <h4>${projects.title}</h4>
+        `<img src="${project.image}"> 
+        <h4>${project.title}</h4>
         <div> 
             <p>Deliverables</p>
-            <p>${projects.deliverables}</p>
+            <p>${project.deliverables}</p>
         </div>
         <a href="#">View Project</a>`
     })
