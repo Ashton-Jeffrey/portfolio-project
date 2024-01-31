e250f3690cdcb50244dc768c2c504813cbeae550
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-01-31 13:57:09 +0000

    created div box for image to go in as a bg image

--- portfolio.js
+++ portfolio.js
@@ -5,16 +5,25 @@ fetch('portfolio.json').then(function (response) {
 }).then(function (data) {
     data.projects.forEach(function (project) {
     portfolioDisplay.innerHTML += 
-        `<div class=" flex flex-col px-20 h-screen" >
-            <img class="object-contain m-auto" src="${project.image}"> 
+        `<div class=" flex flex-col gap-2 py-5 px-20 h-screen" >
+
+            <div style="width:100%; 
+                height:100%; 
+                border:solid;
+                background-image: url("${project.image}");">
+
+            </div>
+
             <div class= "flex flex-wrap justify-between"> 
                 <h4 class="text-pink-500 text-lg font-extrabold">${project.title}</h4>
                 <div class="flex flex-col"> 
-                    <p>Deliverables</p>
-                    <p>${project.deliverables}</p>
+                    <p class="text-xs font-bold">Deliverables</p>
+                    <p class="text-sm font-bold">${project.deliverables}</p>
                 </div>
-                <a href="#">View Project</a>
+                <a class="text-sm font-bold hover:text-pink-500" href="#">View Project</a>
                 </div>
             </div>`
     })
-})
+}) 
+
+// <img class="object-contain w-full" src="${project.image}"> 
