0432b9034602fc2219aeb18e04380c9177fd706f
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-01-31 16:21:12 +0000

    started fetch request code for portfolio page, still WIP

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,24 +1,35 @@
-const portfolioDisplay = document.querySelector('.portfolio')
+const portfolioDisplay = document.querySelector('main')
 
 fetch('js/portfolio.json').then(function (response) {
     return response.json()
 }).then(function (data) {
     data.projects.forEach(function (project) {
     portfolioDisplay.innerHTML += 
-        `<div class="flex flex-col gap-2 py-5 px-20 h-screen">
-
-            <div class="w-full h-full bg-cover rounded-sm" style='background-image: url("${project.image}");'>
+        `<div class="gap-6 p-5 h-screen">
+        <h3 class="text-8xl">
+        ${project.title}
+        </h3>
+        <div class="flex flex-col gap-28"> 
+            <div class="flex gap-40">
+                <div class="w-full h-full bg-cover rounded-sm" style='background-image: url("${project.image}");'>
+                </div>
             </div>
 
-            <div class= "flex flex-wrap justify-between"> 
-                <h4 class="text-pink-500 text-lg font-extrabold">${project.title}</h4>
-                <div class="flex flex-col"> 
-                    <p class="text-xs font-bold">Deliverables</p>
-                    <p class="text-sm font-bold">${project.deliverables}</p>
+            <div class="flex gap-40">
+                <div class="basis-1/4">
+                    <p class="text-lg font-extrabold">Deliverables</p>
+                    <p>${project.deliverables}</p>
                 </div>
-                <a class="text-sm font-bold hover:text-pink-500" href="#">View Project</a>
-                </div>
-            </div>`
+
+                <div class="basis-3/4 flex gap-2 pr-6">
+                    <p>${project.description}</p>
+                </div>   
+            </div>
+
+        </div>
+
+    </div>`    
+    
     })
 }) 
 
