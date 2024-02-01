d3e15b0b30a707ba64989a64c8f76d4439cf444f
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-02-01 14:09:15 +0000

    created a function to link 'view project' buttons to portfolio page

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,14 @@
 const portfolioDisplay = document.querySelector('main')
 
+const requestedProject = window.location.hash.substring(1)
+
 fetch('js/portfolio.json').then(function (response) {
     return response.json()
 }).then(function (data) {
     data.projects.forEach(function (project) {
     portfolioDisplay.innerHTML += 
     `<div class="p-5 min-h-screen">
-        <h3 class="text-8xl pb-10" id="#${project.projectno}">
+        <h3 class="text-8xl pb-10" id="${project.projectno}">
         ${project.title}
         </h3>
         <div class="flex gap-52"> 
@@ -20,7 +22,6 @@ fetch('js/portfolio.json').then(function (response) {
                     <p class="text-lg font-extrabold">Deliverables</p>
                     <p>${project.deliverables}</p>
                 </div>
-             
             </div>
             <div class="flex flex-wrap flex-col gap-20">
    
@@ -31,9 +32,10 @@ fetch('js/portfolio.json').then(function (response) {
                 </div>  
             </div> 
         </div>
-
     </div>`    
     
-    })
+    }) 
+    const scrollTarget = document.querySelector(`#${requestedProject}`)
+    scrollTarget.scrollIntoView()
 }) 
 
