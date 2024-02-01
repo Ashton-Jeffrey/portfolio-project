66d4d9912dbc659ad649382df28bae833d9a37df
Ashton Jeffrey <ashtonjeffrey@live.com>
2024-02-01 11:46:51 +0000

    finished creating js code for portfolio page

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,27 +5,31 @@ fetch('js/portfolio.json').then(function (response) {
 }).then(function (data) {
     data.projects.forEach(function (project) {
     portfolioDisplay.innerHTML += 
-        `<div class="gap-6 p-5 h-screen">
-        <h3 class="text-8xl">
+    `<div class="p-5 min-h-screen">
+        <h3 class="text-8xl pb-10">
         ${project.title}
         </h3>
-        <div class="flex flex-col gap-28"> 
-            <div class="flex gap-40">
-                <div class="w-full h-full bg-cover rounded-sm" style='background-image: url("${project.image}");'>
-                </div>
-            </div>
+        <div class="flex gap-52"> 
 
-            <div class="flex gap-40">
-                <div class="basis-1/4">
+            <div class="flex flex-col justify-center basis-1/2 gap-10">
+                <div> 
+                    <a class="hover:text-pink-500" href="${project.link}" alt="link-to-project-github">Link to GitHub</a>
+                </div>
+
+                <div class="flex flex-col gap-1">
                     <p class="text-lg font-extrabold">Deliverables</p>
                     <p>${project.deliverables}</p>
                 </div>
-
-                <div class="basis-3/4 flex gap-2 pr-6">
-                    <p>${project.description}</p>
-                </div>   
+             
             </div>
-
+            <div class="flex flex-wrap flex-col gap-20">
+   
+                <div class="h-80 bg-center bg-no-repeat rounded-sm" style='background-image: url("${project.image}");'>
+                </div>
+                <div class="pr-6">
+                    <p>${project.description}</p>
+                </div>  
+            </div> 
         </div>
 
     </div>`    
